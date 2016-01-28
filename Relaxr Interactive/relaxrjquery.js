55282eb36c330dd5d275b2cbf2ad90e3a3eff962
Nayana Davis <nayana487@gmail.com>
2016-01-28 14:33:33 -0500

    added comments

--- Relaxr Interactive/relaxrjquery.js
+++ Relaxr Interactive/relaxrjquery.js
@@ -1,3 +1,7 @@
+// ND: So obviously this is working, but it's good practice to encase your code
+// in the document.ready function to make sure your DOM elements load before your
+//JS file 
+
 $('.readmore').click(slideDown);
 
 function slideDown () {
@@ -16,14 +20,16 @@ function slideUP () {
 
 
 
-$('.learnmore').click(slideDown2); 
+$('.learnmore').click(slideDown2);
 
 function slideDown2 () {
 	event.preventDefault();
 	$("#learnmoretext").removeClass("hide2");
 	$(".learnmore").addClass("hide2")
-
+// ND: Got an extra space here you don't need.
 }
 
+// ND: So how would you get the learnmore text to no longer display?
 
-
+ // ND: Nice work, your app is functional. To take it to the next level, try combining your slideDown
+ // functions and use an if statment based on what was clicked. The tighter your code, the better
