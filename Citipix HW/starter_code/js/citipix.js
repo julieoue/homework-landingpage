83130ac51e41837490f3c4b738a665398ffd1060
Joni Halabi <joni@jhalabi.com>
2016-01-30 23:40:33 -0500

    Adding feedback for homework #5.

--- Citipix HW/starter_code/js/citipix.js
+++ Citipix HW/starter_code/js/citipix.js
@@ -1,19 +1,37 @@
+/*
+ * Hi Julie!  I am adding feedback about your code in Javascript comments like this one.
+ *
+ * Overall, fantastic job!!  The page functions perfectly and your code below is very well 
+ * formatted and indented.  The only code style comment that I have is to make sure you stay 
+ * consistent with the number of new lines between bits of your code.
+ */
+
 $(document).ready(function() {
 
 
 
     $("form").submit(updatePic);
     // Entering form updates Picture 
-
+    // Great use of comments here ^^
 
 
 
     function updatePic() {
+
+        /*
+         * Great use of event.preventDefault().  Don't forget to pass the "event" argument to the 
+         * function definition in the line above (to fix issues with some browsers, like we
+         * discussed in class):
+         *
+         *  function updatePic(event) {
+         */
+
         event.preventDefault();
         // Entering city type into the variable 
         var city = $("#city-type").val();
 
         if (city == "New York" || city == "New York City" || city == "NYC") {
+            // Great use of chaining jQuery functions!
             $('body').removeClass().addClass("nyc");
         }
        
